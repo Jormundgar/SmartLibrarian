fcf8d022170872f31baa525aa0710f3c0b757547
Aleksei Volkov <ardoritis@gmail.com>
2023-11-04 03:36:55 +0200

    Update PersonRestController

--- src/main/java/com/volkov/smartlibrarian_boot/controllers/PeopleRestController.java
+++ src/main/java/com/volkov/smartlibrarian_boot/controllers/PeopleRestController.java
@@ -1,24 +1,26 @@
 package com.volkov.smartlibrarian_boot.controllers;
 
+import com.volkov.smartlibrarian_boot.controllers.api.PeopleRestApi;
+import com.volkov.smartlibrarian_boot.dto.PersonDTO;
 import com.volkov.smartlibrarian_boot.services.PersonService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 @AllArgsConstructor
 @Slf4j
-public class PeopleRestController {
+public class PeopleRestController implements PeopleRestApi {
 
     private final PersonService personService;
 
-    @RequestMapping(value = "/people/all", produces = "application/json", method = RequestMethod.GET)
-    public ResponseEntity<?> index() {
+    @Override
+    public ResponseEntity<List<PersonDTO>> getAll() {
         var users = personService.findAllDTOs();
         log.info("Total records from index() method to return: " + users.size());
         return new ResponseEntity<>(users, HttpStatus.OK);
--- src/main/java/com/volkov/smartlibrarian_boot/controllers/api/PeopleRestApi.java
+++ src/main/java/com/volkov/smartlibrarian_boot/controllers/api/PeopleRestApi.java
@@ -1,35 +1,42 @@
 package com.volkov.smartlibrarian_boot.controllers.api;
 
-import com.volkov.smartlibrarian_boot.dto.BookDTO;
 import com.volkov.smartlibrarian_boot.dto.PersonDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-//@Api(tags = "People REST")
-//@Tag(name = "People REST", description = "People API description")
-//@RequestMapping("/api/people")
-//public interface PeopleRestApi {
-//
-//    @GetMapping
-//    @ApiOperation(value = "Get all People")
-//    @ApiResponses(value = {
-//            @ApiResponse(code = 200, message = "People found"),
-//            @ApiResponse(code = 204, message = "People not present")}
-//    )
-//    ResponseEntity<List<PersonDTO>> getAll();
-//
+@RequestMapping("/api/people")
+@Tag(name = "Users REST", description = "API for managing readers")
+public interface PeopleRestApi {
+
+    @Operation(
+            summary = "Get all Readers",
+            description = "Retrieve a list of all readers",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "List of readers retrieved successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = PersonDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "No readers found"
+                    )
+            }
+    )
+    @GetMapping
+    ResponseEntity<List<PersonDTO>> getAll();
+
 //    @GetMapping("/{id}")
 //    @ApiOperation(value = "Get Person by id")
 //    @ApiResponses(value = {
@@ -66,4 +73,4 @@ import java.util.List;
 //            @ApiResponse(code = 404, message = "Person not found")}
 //    )
 //    ResponseEntity<Void> delete(@ApiParam(name = "id", value = "Person.id") @PathVariable(value = "id") Integer id);
-//}
+}
