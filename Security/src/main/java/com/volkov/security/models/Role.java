3cca546716e58803eae5e3ae5bed9940401a6f8b
Aleksei Volkov <ardoritis@gmail.com>
2023-11-27 19:48:28 +0200

    Security models added

--- Security/src/main/java/com/volkov/security/models/Role.java
+++ Security/src/main/java/com/volkov/security/models/Role.java
@@ -1,11 +1,40 @@
 package com.volkov.security.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.*;
 
 @Entity
-public class Role {
+@Table(name = "roles")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role implements GrantedAuthority {
 
     @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    @Override
+    public String getAuthority() {
+        return null;
+    }
 }
--- Security/src/main/java/com/volkov/security/models/User.java
+++ Security/src/main/java/com/volkov/security/models/User.java
@@ -1,50 +1,85 @@
 package com.volkov.security.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Fetch;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.GenerationType.*;
+import static org.hibernate.annotations.FetchMode.SUBSELECT;
 
 @Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
 public class User implements UserDetails {
 
     @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(cascade = ALL)
+    @Fetch(SUBSELECT)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return roles;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
