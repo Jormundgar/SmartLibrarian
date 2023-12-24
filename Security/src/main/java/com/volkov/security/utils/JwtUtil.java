79922bff2ca810cd04541cb27446e8ccf24d95dc
Aleksei Volkov <ardoritis@gmail.com>
2023-12-24 18:31:55 +0200

    Start JWT Util

--- Security/src/main/java/com/volkov/security/utils/JwtUtil.java
+++ Security/src/main/java/com/volkov/security/utils/JwtUtil.java
@@ -1,7 +1,35 @@
-//package com.volkov.security.utils;
-//
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class JwtUtil {
-//}
+package com.volkov.security.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt_secret}")
+    private String secret;
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        var expirationDate = Date.from(ZonedDateTime.now().plusMinutes(60).toInstant());
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(expirationDate)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
