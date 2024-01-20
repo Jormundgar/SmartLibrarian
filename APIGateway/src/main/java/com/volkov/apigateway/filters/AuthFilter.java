cce2332ca9c55b36b8f13b7e1a5a02f27726396f
Aleksei Volkov <ardoritis@gmail.com>
2024-01-20 21:18:07 +0200

    AuthFilter in progress

--- APIGateway/src/main/java/com/volkov/apigateway/filters/AuthFilter.java
+++ APIGateway/src/main/java/com/volkov/apigateway/filters/AuthFilter.java
@@ -1,4 +1,33 @@
 package com.volkov.apigateway.filters;
 
-public class AuthFilter {
+import com.volkov.apigateway.util.AuthUtil;
+import com.volkov.apigateway.util.JWTUtil;
+import com.volkov.apigateway.validator.RouteValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@RefreshScope
+@RequiredArgsConstructor
+public class AuthFilter implements GatewayFilter {
+
+    private final RouteValidator routeValidator;
+    private final JWTUtil jwtUtil;
+    private final AuthUtil authUtil;
+
+    @Value("${authentication.enabled}")
+    private boolean authEnabled;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+
+
 }
--- APIGateway/src/main/java/com/volkov/apigateway/util/AuthUtil.java
+++ APIGateway/src/main/java/com/volkov/apigateway/util/AuthUtil.java
@@ -1,4 +1,7 @@
 package com.volkov.apigateway.util;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AuthUtil {
 }
