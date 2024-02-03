d75f4fe1b9e532bcb2f14805ef3b48d6cb50c3ae
Aleksei Volkov <ardoritis@gmail.com>
2024-02-03 02:40:52 +0200

    AuthFilter Add

--- APIGateway/src/main/java/com/volkov/apigateway/filters/AuthFilter.java
+++ APIGateway/src/main/java/com/volkov/apigateway/filters/AuthFilter.java
@@ -3,10 +3,11 @@ package com.volkov.apigateway.filters;
 import com.volkov.apigateway.util.JWTUtil;
 import com.volkov.apigateway.validator.RouteValidator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -19,13 +20,40 @@ public class AuthFilter implements GatewayFilter {
     private final RouteValidator routeValidator;
     private final JWTUtil jwtUtil;
 
-    @Value("${authentication.enabled}")
-    private boolean authEnabled;
-
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return null;
+        var request = exchange.getRequest();
+        if (routeValidator.isSecured.test(request)) {
+            if (this.isAuthMissing(request)) {
+                return this.onError(exchange, HttpStatus.UNAUTHORIZED);
+            }
+            final var token = this.getAuthHeader(request);
+            if (jwtUtil.isInvalid(token)) {
+                return this.onError(exchange, HttpStatus.FORBIDDEN);
+            }
+            this.updateRequest(exchange, token);
+        }
+        return chain.filter(exchange);
     }
 
+    private Mono<Void> onError(ServerWebExchange exchange, HttpStatus httpStatus) {
+        var response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        return response.setComplete();
+    }
 
+    private String getAuthHeader(ServerHttpRequest request) {
+        return request.getHeaders().getOrEmpty("Authorization").get(0);
+    }
+
+    private boolean isAuthMissing(ServerHttpRequest request) {
+        return !request.getHeaders().containsKey("Authorization");
+    }
+
+    private void updateRequest(ServerWebExchange exchange, String token) {
+        var claims = jwtUtil.getAllClaimsFromToken(token);
+        exchange.getRequest().mutate()
+                .header("username", String.valueOf(claims.get("username")))
+                .build();
+    }
 }
